3abf562306c34e3a78e0fac7408b584ac61ef334
phytoclast2 <76961867+phytoclast2@users.noreply.github.com>
2021-08-11 16:27:46 -0400

    Update get_spatial_soil.R

--- get_spatial_soil.R
+++ get_spatial_soil.R
@@ -22,20 +22,21 @@ obs <- subset(obs,Observer_Code %in% 'GRR.GJS' & Year >=2011 & !Observation_Type
 obs.geom <- st_as_sf(obs[,c('Observation_ID', 'Longitude', 'Latitude')], coords = c('Longitude', 'Latitude'))
 st_crs(obs.geom) <-  st_crs('epsg:4326')
 obs.geom2 <-  as_Spatial(obs.geom)
-ssurgo=NA
-for (i in 1:nrow(obs.geom2)){#i=400
+ssurgo=NULL
+ssurgo.i<-NULL
+for (i in 1:nrow(obs.geom2)){#i=400 length(db)
   obs.geom.i <- obs.geom2[i,]
   ssurgo.i <- SDA_spatialQuery(
     obs.geom.i,
     what = "mukey",
     geomIntersection = FALSE,
-    db = c("SSURGO", "STATSGO")
+    db = c("SSURGO")
   )
   if(!is.null(ssurgo.i)){
     ssurgo.i <-  cbind(list(obs.id = obs.geom.i$Observation_ID[1]), ssurgo.i)}
   else{
     ssurgo.i <-  list(obs.id = obs.geom.i$Observation_ID[1], mukey = NA, muname = NA)}
-  if(is.na(ssurgo)){
+  if(is.null(ssurgo)){
     ssurgo <- ssurgo.i
   }else{
     ssurgo <- rbind(ssurgo,ssurgo.i)
